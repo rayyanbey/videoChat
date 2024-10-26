d5d7e108ba65dfe0b4cc14eec6385fbc21e5d7f4
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2024-10-26 19:05:25 +0500

    Refactor Image component to use a specific image file

--- frontend/src/components/Image.jsx
+++ frontend/src/components/Image.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import {image} from '../'
+import image from '../assets/blue.png'
 const HQImage = () => {
   return (
     <div className='flex items-center justify-center top-0'>
-      <img src="../public/blue.png" alt="" className='w-[200px] h-1/6 top-0' />
+      <img src={image} alt="" className='w-[200px] h-1/6 top-0' />
     </div>
   )
 }
